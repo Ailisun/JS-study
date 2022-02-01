2766f264966110fb612cbd70f9a85a076614f2b3
Ailisun <13prichin@gmail.com>
2022-02-01 21:51:52 +0200

    Classwork, lesson 1

---
+++ JS-lessons/lesson1/classwork/scriptt.js
@@ -0,0 +1,36 @@
+let arr = [10, 22, 55, 245, 33, 99, 43, 876, 11, 2]
+let result = arr[0] + arr[1] + arr[2] + arr[3]+ arr[4]+ arr[5]+ arr[6]+ arr[7]+ arr[8]+ arr[9]
+console.log(result)
+let book1 = {
+    title: "Harry Potter",
+    pageQuantity: 300,
+    genre: "fantastic"
+}
+
+let book2 = {
+    title: "Recipes",
+    pageQuantity: 300,
+    genre: "cooking",
+    authors: ["Antoni Parowski", "Kim Joy"]
+
+}
+let books = [book1, book2]
+console.log(books[0])
+console.log(books[1])
+
+let height = 23;
+let width = 10;
+let square = height*width
+console.log(square)
+
+let heightC = 10;
+let dC = 4;
+let rC = dC*0.5
+console.log(rC)
+let v= Math.PI*Math.pow(rC,2)*heightC
+console.log(v)
+
+let n = 3;
+let m = 4;
+let c = n**2+m**2
+console.log(c)
