1a075ec3ba5aea407bf1a4a3bc52db23941a352e
Ailisun <13prichin@gmail.com>
2022-02-01 20:56:56 +0200

    homework 1

---
+++ JS-lessons/lesson1/homework/script.js
@@ -0,0 +1,78 @@
+// let hello = 'hello';
+// let name = 'owu';
+// let site = 'com';
+// let mova = 'ua';
+// let first = 1;
+// let  ten = 10;
+// let negativeNumber = -999;
+// let tres = 123;
+// let p = 3.14;
+// let ailer = 2.7;
+// let sixteen = 16;
+// let pravda = true;
+// let isCrap = false;
+// let arr = [hello, name, site, mova, first, ten, negativeNumber, tres, p, ailer, sixteen, pravda, isCrap]
+// console.log(hello, name, site, mova, first, ten, negativeNumber, tres, p, ailer, sixteen, pravda, isCrap)
+// alert (arr)
+// document.write("<h1>" + arr + "</h1>")
+// name = 'only';
+//
+// site = 'fans';
+// mova = 'en';
+// first = 11;
+// ten = 100;
+// negativeNumber = 666;
+// tres = 321;
+// p = 42.3;
+// ailer = 7.2;
+// sixteen = 61;
+// pravda = false;
+// isCrap = true;
+//
+// let arr2 = [hello, name, site, mova, first, ten, negativeNumber, tres, p, ailer, sixteen, pravda, isCrap]
+// alert (arr2)
+// console.log(arr2)
+// document.write(arr2)
+//
+// let firstName = 'Olha';
+// let middleName = 'Tarasivna';
+// let lastName = 'Lazarenko'
+// let person = firstName + ' ' + middleName + ' '+ lastName
+// console.log(person)
+
+// let age = 22;
+// let hh = prompt('please enter your name', firstName)
+// let nb = prompt ('here should be middle name', middleName)
+// let rr = prompt('your age', age)
+
+// alert("Hi" + ' ' + hh + ' ' + nb + ' ' + '.' + " You are" + ' ' + ' ' + rr)
+
+// let a = 100;
+// let b = '100';
+// let c = true;
+// console.log(typeof a)
+// console.log(typeof b)
+// console.log(c)
+
+// console.log(5<6)
+// console.log(5 > 6)
+// console.log(5 === 6)
+// console.log(5>=6)
+// console.log(10 === 10)
+// console.log( 10 >= 10)
+// console.log( 10 !== 10)
+// console.log( 10  > 10)
+// console.log( 10 < 10)
+// console.log( 123 === '123')
+// console.log( 123 >= '123')
+
+let str = "20";
+let a = 5;
+// document.write(parseInt (str) + a + "<br/>");
+document.write(str + a + "<br/>");
+document.write(str - a + "<br/>"); //number
+document.write(str * "2" + "<br/>");// тут переобразование в число
+document.write(str / 2 + "<br/>");//number
+
+
+
