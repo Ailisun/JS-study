9dc0f58597698f8b2d38a5e0610e33f0d79de8a9
Ailisun <13prichin@gmail.com>
2022-02-18 19:16:42 +0200

    additional as#1

---
+++ JS-lessons/lesson7/additional/scriptAd.js
@@ -0,0 +1,45 @@
+function Users (id, name,username, email, address, phone, website,company){
+    this.id = id;
+    this.name = name;
+    this.username =username;
+    this.email = email;
+    this.address = address;
+    this.phone = phone;
+    this.website = website;
+    this.company = company
+}
+class Address {
+    street;
+    suite;
+    city;
+    geo;
+
+    constructor(street, suite, city, geo) {
+        this.street = street;
+        this.suite = suite;
+        this.city = city;
+        this.geo  = geo;
+    }
+}
+class Company {
+    name;
+    catchPhrase;
+    bs;
+
+    constructor(name, catchPhrase, bs) {
+        this.name = name;
+        this.catchPhrase = catchPhrase;
+        this.bs = bs;
+    }
+}
+class Geo {
+    lat;
+    lng;
+
+    constructor(lat, lng) {
+        this.lat = lat;
+        this.lng = lng;
+    }
+}
+let user = new Users(908, 'olha', 'laza', '13pr@gmso', new Address('Lviv', '144', 'Stryiska', new Geo('938','o376')),'08934t2', 'okten', new Company('namecompany', 'phrase phrase','Що таке bs?')  )
+console.log(user)
