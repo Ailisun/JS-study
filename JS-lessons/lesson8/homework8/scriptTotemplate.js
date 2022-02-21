324855151b4dd21e06f5837a07cf87a5b4621c11
Ailisun <13prichin@gmail.com>
2022-02-21 16:53:45 +0200

    homework done

---
+++ JS-lessons/lesson8/homework8/scriptTotemplate.js
@@ -0,0 +1,38 @@
+// Візьміть файл template1.html, підключіть до нього скрипт, і працюйте в ньому!
+//
+//     - Напишіть код,  котрий :
+// -- отримує текст з параграфа з id "content"
+// let textInId = document.getElementById('content')
+// console.log(textInId)
+
+// -- отримує текст з блоку з id "rules"
+// let textInId1 = document.getElementById('rules')
+// console.log(textInId1)
+
+// -- замініть текст параграфа з id 'content' на будь-який інший
+// let textInId = document.getElementById('content')
+// textInId.innerHTML = 'Весна — переходный сезон, когда заметно увеличение светового дня, повышается температура окружающей среды и активизируется природная деятельность живых существ и растений.'
+
+// -- замініть текст параграфа з id 'rules' на будь-який інший
+// let textInId1 = document.getElementById('rules')
+// textInId1.innerHTML = 'Spring, also known as springtime, is one of the four temperate seasons, succeeding winter and preceding summer.'
+
+// -- змініть кожному елементу колір фону на червоний
+// let changeColor = document.body.children
+// for (const changeColorElement of changeColor) {
+//     changeColorElement.style.backgroundColor = 'red'}
+
+// -- змініть кожному елементу колір тексту на синій
+// let changeColor = document.body.children
+// for (const changeColorElement of changeColor) {
+//     changeColorElement.style.color = 'blue'}
+
+// -- отримати весь список класів елемента з id=rules і вивести їх в console.log
+// let listCreation = document.getElementById('rules')
+// console.log(listCreation.classList)
+
+// -- поміняти колір тексту у всіх елементів fc_rules на червоний
+// let newColor = document.body.getElementsByClassName('fc_rules')
+// for (const item of newColor) {
+// item.style.color = 'red'
+// }
