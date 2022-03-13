25a4cc00b97d7569dd606e9afc40678d7af90697
Ailisun <13prichin@gmail.com>
2022-03-14 00:26:49 +0200

    done

--- JS-lessons/lesson12/homework/script_h12.js
+++ JS-lessons/lesson12/homework/script_h12.js
@@ -2,7 +2,7 @@
 // Застилізувати, за допомоги css, щоб отримати 5 елементів в рядку.
 // Для кожного елементу свій блок div.post
 // Всі характеристики повинні мати свої блоки всередені div.post
-// https://jsonplaceholder.typicode.com/posts
+https://jsonplaceholder.typicode.com/posts
 fetch('https://jsonplaceholder.typicode.com/posts')
 .then(response => response.json())
 .then(elements => {
@@ -21,4 +21,23 @@ fetch('https://jsonplaceholder.typicode.com/posts')
 //     Для кожного елементу свій блок div.comment
 // Всі характеристики повинні мати свої блоки всередені div.comment
 // https://jsonplaceholder.typicode.com/comments
+fetch('https://jsonplaceholder.typicode.com/comments')
+.then(response => response.json())
+.then(elements => {
+    let comments = document.createElement('div')
+    comments.classList.add('main')
+    for (const element of elements) {
+        let divComment = document.createElement('div')
+        divComment.classList.add ('subsection')
+        divComment.innerHTML = `<div class="both">
+        <h4>${element.postId}.</h4> 
+        <h4>${element.id}</h4></div>
+        <h5>${element.name}</h5> 
+        <h5>${element.email}</h5>  
+        <p>${element.body}`
+        comments.append(divComment)
+        document.body.appendChild(comments)
+
+    }
+})
 
