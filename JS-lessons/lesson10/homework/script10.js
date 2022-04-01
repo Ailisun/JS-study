403b00a45edfc1ea552cd7a360ab67e3c0c97b46
Ailisun <13prichin@gmail.com>
2022-04-01 11:31:26 +0300

    in progress

--- JS-lessons/lesson10/homework/script10.js
+++ JS-lessons/lesson10/homework/script10.js
@@ -16,47 +16,49 @@
 // }
 
 // створити інпут який приймає вік людини та кнопку яка підтверджує дію.
-//     При натисканні на кнопку зчитати інформацію з інпуту та перевірити вік чи меньше він ніж 18, та повідомити про це користувача
-// let form = document.forms[0]
-// let inputData = form.age;
-// inputData.addEventListener('input', (e) => {
-//     let yourInput = e.target
-//     let DatafromInput = yourInput.value
-//     if (DatafromInput < 18) {
-//         alert('please leave this page')
+//При натисканні на кнопку зчитати інформацію з інпуту та перевірити вік чи меньше він ніж 18, та повідомити про це користувача
+
+
+// document.getElementById('btn').onclick = function () {
+//   let age =   document.getElementById('age').value;
+//      if (+age < 18) {
+//         alert('You're not old enough for this');
+//     } else {
+//         alert('Welcome, buddy');
 //     }
-// // })
+// }
 // // - Создайте меню, которое раскрывается/сворачивается при клике
 
-// let ulElemenet = document.getElementsByClassName("menu");
-// let titleElement = document.getElementsByClassName("title");
-// titleElement.onclick = function(){
-//     ulElemenet.classList.toggle("hidden");
-// }
+let ulElemenet = document.getElementsByClassName("menu");
+let titleElement = document.getElementsByClassName("title");
+titleElement.onclick = function(){
+    ulElemenet.classList.toggle("hidden");
+}
 // Создать список комментариев , пример объекта коментария - {title : 'lorem', body:'lorem ipsum dolo sit ameti'}.
 //     Вывести список комментариев в документ, каждый в своем блоке.
 //     Добавьте каждому комментарию по кнопке для сворачивания его body.
-let comments = [
-    { title: ' Good Morning', body:'could you make me coffee?'},
-    { title: ' Lunch', body:'I have to work'},
-    { title: ' Night', body:'Wanna kanapku'}
-]
-let general = document.createElement('div')
-for (const elem of comments) {
-    let createBlock = document.createElement('div')
-    let titleShow = document.createElement('h3')
-    let commentShow = document.createElement('p')
-    let buttonShow = document.createElement('button')
-
-   titleShow.innerHTML = elem.title
-   commentShow.innerHTML = elem.body
-    buttonShow.innerHTML = 'close me'
-
-    buttonShow.onclick = function (){
-        commentShow.classList.add('pHidden');
-    }
-    createBlock.append(titleShow,commentShow,buttonShow)
-    general.append(createBlock)
-}
-document.body.append(general)
+// let comments = [
+//     { title: ' Good Morning', body:'could you make me coffee?'},
+//     { title: ' Lunch', body:'I have to work'},
+//     { title: ' Night', body:'Wanna kanapku'}
+// ]
+// let general = document.createElement('div')
+// for (const elem of comments) {
+//     let createBlock = document.createElement('div')
+//     let titleShow = document.createElement('h3')
+//     let commentShow = document.createElement('p')
+//     commentShow.classList.add('xxx')
+//     let buttonShow = document.createElement('button')
+//
+//    titleShow.innerHTML = elem.title
+//    commentShow.innerHTML = elem.body
+//     buttonShow.innerHTML = 'close me'
+//
+//     buttonShow.onclick = function (){
+//
+//     }
+//     createBlock.append(titleShow,commentShow,buttonShow)
+//     general.append(createBlock)
+// }
+// document.body.append(general)
 //спросить у Тараса почему не работает
