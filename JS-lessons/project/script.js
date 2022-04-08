a26b021f1ce9807ead4c9748fea29f9b92a8d6ae
Ailisun <13prichin@gmail.com>
2022-04-08 03:10:30 +0300

    in progress

--- JS-lessons/project/script.js
+++ JS-lessons/project/script.js
@@ -21,3 +21,18 @@
 // post-details.html - блок с информацией про пост вверху. Комментарии - по 4 в ряд.
 // Все без исключения элементы, который характеризируют user,post,comment  визуализировать, так, что бы было видно их блоки (дать задний фон + margin. Иными словами - крайне четкая сетка)
 //
+
+let usersContainer = document.getElementsByClassName('users')[0]
+fetch('https://jsonplaceholder.typicode.com/users')
+       .then(value => value.json())
+       .then (value => {
+           for (const userItem of value) {
+               let userContainer = document.createElement('div')
+               userContainer.classList.add('user')
+               let anchor = document.createElement('a')
+               anchor.innerText = `${userItem.id} - ${userItem.name}`
+               anchor.href = `./user-details.html?data=${JSON.stringify(userItem)}`
+               userContainer.appendChild(anchor)
+               usersContainer.appendChild(userContainer)
+           }
+       })
