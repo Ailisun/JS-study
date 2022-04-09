12277a2d84e8aa4e0cb3e909aa09efffe52f310c
Ailisun <13prichin@gmail.com>
2022-04-10 01:35:06 +0300

    done

--- JS-lessons/project/post-details.js
+++ JS-lessons/project/post-details.js
@@ -2,7 +2,10 @@ let url = new URL(location.href)
 let post = JSON.parse(url.searchParams.get('data'))
 console.log(post)
 let postContainer = document.getElementsByClassName('postAdd')[0]
-postContainer.innerText = JSON.stringify(post)
+postContainer.innerHTML =` <h4>Id: ${post.id}</h4>
+ <h4>User number: ${post.userId}</h4>
+ <h4>Title: ${post.title}</h4>
+ <h4>Post: ${post.body}</h4>`
 let commentsContainer = document.getElementsByClassName('comments')[0]
 fetch(`https:jsonplaceholder.typicode.com/posts/${post.id}/comments`)
     .then (value => value.json())
